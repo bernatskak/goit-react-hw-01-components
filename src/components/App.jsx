a590cdeb51e68fadd381c128fe82e40d77671009
bernatskak <106559001+bernatskak@users.noreply.github.com>
2023-01-22 02:30:06 +0200

    finish

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,31 @@
-export const App = () => {
+import { Profile } from './profile/Profile';
+import user from './profile/user.json';
+
+import { Statistics } from './statistics/Statistics';
+import data from './statistics/data.json';
+
+import { FriendList } from './friend-list/FriendList';
+import friends from './friend-list/friends.json';
+
+import { TransactionHistory } from './transaction-history/TransactionHistory';
+import transactions from './transaction-history/transactions.json';
+
+export function App() {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <>
+      <div>
+        <Profile
+          username={user.username}
+          tag={user.tag}
+          location={user.location}
+          avatar={user.avatar}
+          stats={user.stats}
+        />
+        <Statistics title="Upload stats" stats={data} />
+        <Statistics stats={data} />
+        <FriendList friends={friends} />
+        <TransactionHistory items={transactions} />
+      </div>
+    </>
   );
-};
+}
